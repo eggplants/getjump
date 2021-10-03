867ce3f6fdd257c3f5700b3e5aa31836b88bcb92
eggplants <w10776e8w@yahoo.co.jp>
2021-10-03 21:44:43 +0900

    v0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="getjump",
-    version="0.2",
+    version="0.3",
     description="Get and save images from jump web viewer",
     description_content_type="",
     long_description=open("README.md").read(),
