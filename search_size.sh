4696e8b502c88710d0f98e900bb98c2cc3b24806
eggplants <w10776e8w@yahoo.co.jp>
2021-10-04 11:28:35 +0900

    fix: add usage to script to searching size

--- search_size.sh
+++ search_size.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
+# Usage
+# $ ./search_size.sh >_
+# $ sed -n '/width/p;/height/p' _ | sort | uniq
+
 for site in shonenjumpplus comic-days; do
   base="https://${site}.com"
-  curl -s "$base" | grep -oP "${base}/episode/\d+" |
+  {
+  curl -s "$base"
+  curl -s "${base}/series"
+  } | grep -oP "${base}/episode/\d+" | sort | uniq |
   while read episode;do
     echo "${episode}.json"
     curl -s "${episode}.json" | grep -oP '("height"|"width"):\d+' |
