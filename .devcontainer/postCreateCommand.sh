7554aab9dd804841c93f82511110ca44ee4873d2
eggplants <w10776e8w@yahoo.co.jp>
2026-04-25 10:49:31 +0000

    chore: mise

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,19 +2,20 @@
 
 set -exo pipefail
 
-if ! grep -q mise ~/.zshrc; then
-  # shellcheck disable=SC2016
-  echo 'eval "$(mise activate zsh)"' >> ~/.zshrc
-fi
-
 mise trust
 mise install
 mise generate git-pre-commit --write
 eval "$(mise activate zsh)"
 
 cat<<'A'>> ~/.zshrc
+eval "$(mise activate zsh)"
+
+eval "$(mise completion zsh)"
 eval "$(uv generate-shell-completion zsh)"
 eval "$(uvx --generate-shell-completion zsh)"
+
+autoload -U compinit
+compinit -i
 A
 
 sed -i ~/.zshrc -e 's/^ZSH_THEME=.*/ZSH_THEME="refined"/'
